e62367fffcf5f819415ebd156aabdb1bb50b4bc4
Mohamad <mo@julia>
2023-08-15 22:31:52 -0400

    adding docker

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
-sudo docker build -t hallawalla/paralleldg:0.9.2 . --no-cache
-# sudo docker run -t hallawalla/paralleldg:0.7 
-sudo docker push  hallawalla/paralleldg:0.9.2
+sudo docker build -t hallawalla/paralleldg:0.9.5 . --no-cache
+#sudo docker run -t hallawalla/paralleldg:0.9.5
+sudo docker push  hallawalla/paralleldg:0.9.5
 
 
